d82b65f80fa76b4c706c782499f8e362250b29a1
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-11 15:38:51 +0500

    file reading

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+    fstream file;
+    string line;
+    file.open(argv[1], ios::in);
+    if (file.is_open()) {
+        while ( getline (file, line) ) {
+             cout << line << '\n';
+        }
+        file.close();
+    }
     cout <<"hello\n";
     return 0;
 }
