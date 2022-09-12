5aed2e4c49faea7416b568b2dfc793a6b86788e1
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-12 19:33:19 +0500

    biolerplate

--- main.cpp
+++ main.cpp
@@ -1,77 +1,28 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+#include "parser.h"
 using namespace std;
 
-#define TOKEN_IDENTIFIER "I"
-#define TOKEN_NUMBER  "N"
-#define TOKEN_VARIABLE "V"
-#define TOKEN_FUNCTION "F"
-#define TOKEN_INT "n"
-#define TOKEN_IF "i"
-#define TOKEN_ELSE "e"
-#define TOKEN_DO "d"
-#define TOKEN_UNTIL "u"
-#define TOKEN_THEN "t"
-#define TOKEN_READ "r"
-#define TOKEN_DISPLAY "d"
-#define TOKEN_DISPLAYLINE "D"
-#define TOKEN_RETURN "R"
-#define TOKEN_EQUAL "="
-#define TOKEN_NOTEQUAL "!="
-#define TOKEN_LESS "<"
-#define TOKEN_LESSEQUAL "<="
-#define TOKEN_GREATER ">"
-#define TOKEN_GREATEREQUAL ">="
-#define TOKEN_COMMA ","
-#define TOKEN_COLON ":"
-#define TOKEN_SEMICOLON ";"
-#define TOKEN_DOLLAR "$"
-#define TOKEN_OPENPARANTHESIS "("
-#define TOKEN_CLOSEPARANTHESIS ")"
-#define TOKEN_PLUS "+"
-#define TOKEN_MINUS "-"
-#define TOKEN_DIVIDE "/"
-#define TOKEN_MULTIPLY "*"
-#define TOKEN_MODULUS "%"
-
-vector<string> raw;
-
-static void lex(){
-    for (auto item : raw) {
-        for (int j = 0; j < item.length(); ++j){
-            if(item[j]==' ' || item[j]=='\t')
-                continue;
-            else if(item[j]=='\n')
-                break;
-        }
-    }
-}
-
-int main(int argc, char *argv[]){
-    if(argc != 2){
-        cout << "Not enough arguments \n";
-        return 1;
-    }
-    fstream file;
-    string fileName = argv[1];
-    string line;
-    int pos = fileName.find(".");
-    string sub = fileName.substr(pos + 1);
-    if(sub != "cos"){
-        cout << "File must be .cos extension!\n";
-        return 2;
-    }
-    file.open(argv[1], ios::in);
-    if (file.is_open()) {
-        while ( getline (file, line) ) {
-             raw.push_back(line);
-        }
-        file.close();
-    }
-    for(auto it = raw.begin(); it != raw.end(); it++){
-        cout<<*it<<endl;
-    }
-    return 0;
+int main(int argc, char* argv[])
+{
+	//asking for file name as command line arguments
+	if (argc == 2)
+	{
+		lexer _lexer(argv[1]);
+		token t;
+		t = _lexer.getNextToken();
+		while (t.tokenType != TokenType::END_OF_FILE)
+		{
+			t.Print();
+			t = _lexer.getNextToken();
+		}
+	}
+	else if (argc > 2)
+	{ //argument limit exceeds
+		cout << "Too many arguments" << endl;
+	}
+	else //if file name is'nt given
+	{
+		cout << "Please provide a file name" << endl;
+	}
+	return 0;
 }
---
+++ sample_code.ol
@@ -0,0 +1,33 @@
+calculateFib: int <- function($n: int){
+	#~variable declarations, note z is not initialized~#
+    $x:int = 0;
+    $y:int = 0;
+    $z:int;
+
+    if (n -lt 0) then {
+        displayline:'negative number entered';
+        return:0;
+    else if (n -eq 0) then {
+        displayline:"it's 0 man!"
+        return:0;
+    } else {
+        display:"\nThe fibonacci series: ";
+        $i:int = 1;
+        do until($i-1 -ge $n){
+            display:x," ";
+            $z = $x + $y;
+            $x = $y;
+            $y = $z;
+            $i = $i + 1;
+        }
+    }
+    return:0;
+}
+
+#~ program to calculate fibonacci series and display on stdout ~#
+main: int <- function(){
+    $num: int;
+    "Enter the number" :read: $num; #~ input ~#
+    $result: int = execute calculateFib($num)
+    return:0; #~halt~#
+}
